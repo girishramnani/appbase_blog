73a60ce57a5a00a93d517d4588e6606e4b6fdad9
girishramnani <girishramnani95@gmail.com>
2015-11-06 20:46:08 +0530

    added the templates and index route

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express  = require("express");
 
 var app = express();
-
+var express_hbs = require('express-handlebars');
 app.use(express.static(__dirname+"/public"));
 
 
-app.use
+app.engine('hbs', express_hbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
 
+app.get("/",function(req,res){
+
+  res.render("index");
+
+});
 
 app.listen(8000);
