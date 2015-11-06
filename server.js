5a4384a4079ecf976b51779dfeb5fa1ca6b380ec
girishramnani <girishramnani95@gmail.com>
2015-11-06 21:16:40 +0530

    added appbase credentials from parent directory (not in mood to use tree filter to remove commit history )

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express  = require("express");
-
-var app = express();
 var express_hbs = require('express-handlebars');
+var Appbase = require('appbase-js');
+var config = require("../appbaseConfig");
+
+
+
+
+
+// use you appbase credentials here
+var appbase = new Appbase(config);
+
+
+var app = express(config);
 app.use(express.static(__dirname+"/public"));
-
-
 app.engine('hbs', express_hbs({extname:'hbs', defaultLayout:'main.hbs'}));
 app.set('view engine', 'hbs');
 
